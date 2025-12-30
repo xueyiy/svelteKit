a32bc97045dae93d94137ab21ee728495c623fc8
xueyiyang <xueyiyang@smartz.cloud>
2025-12-30 14:48:31 -0600

    add

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,101 +1,33 @@
-<script context="module">
-	export async function load({ page, fetch }) {
-		const [{ articles, pages }, { tags }] = await Promise.all([
-			fetch(`/articles.json?${page.query}`, { credentials: 'include' }).then((r) => r.json()),
-			fetch('/tags.json').then((r) => r.json())
-		]);
-
-		return {
-			props: {
-				articles,
-				pages,
-				tags
-			}
-		};
-	}
-</script>
-
 <script>
-	import { page, session } from '$app/stores';
-	import ArticleList from '$lib/ArticleList/index.svelte';
-	import Pagination from '$lib/Pagination.svelte';
-
-	export let articles;
-	export let pages;
-	export let tags;
-
-	$: p = +$page.query.get('p') || 1;
-	$: tag = $page.query.get('tag');
-	$: tab = $page.query.get('tab') || 'all';
-	$: page_link_base = tag ? `tag=${tag}` : `tab=${tab}`;
+	import { session } from '$app/stores';
 </script>
 
 <svelte:head>
-	<title>Conduit</title>
+	<title>SvelteKit Test App</title>
 </svelte:head>
 
 <div class="home-page">
-	{#if !$session.user}
-		<div class="banner">
-			<div class="container">
-				<h1 class="logo-font">conduit</h1>
-				<p>A place to share your knowledge.</p>
-			</div>
+	<div class="banner">
+		<div class="container">
+			<h1 class="logo-font">SvelteKit Test App</h1>
+			<p>Simple SvelteKit project testing</p>
 		</div>
-	{/if}
+	</div>
 
 	<div class="container page">
 		<div class="row">
-			<div class="col-md-9">
-				<div class="feed-toggle">
-					<ul class="nav nav-pills outline-active">
-						<li class="nav-item">
-							<a
-								href="/?tab=all"
-								rel="prefetch"
-								class="nav-link"
-								class:active={tab === 'all' && !tag}
-							>
-								Global Feed
-							</a>
-						</li>
-
-						{#if $session.user}
-							<li class="nav-item">
-								<a href="/?tab=feed" rel="prefetch" class="nav-link" class:active={tab === 'feed'}>
-									Your Feed
-								</a>
-							</li>
-						{:else}
-							<li class="nav-item">
-								<a href="/login" rel="prefetch" class="nav-link">Sign in to see your Feed </a>
-							</li>
-						{/if}
-
-						{#if tag}
-							<li class="nav-item">
-								<a href="/?tag={tag}" rel="prefetch" class="nav-link active">
-									<i class="ion-pound" />
-									{tag}
-								</a>
-							</li>
-						{/if}
-					</ul>
-				</div>
-
-				<ArticleList {articles} />
-				<Pagination {pages} {p} href={(p) => `/?${page_link_base}&page=${p}`} />
-			</div>
-
-			<div class="col-md-3">
-				<div class="sidebar">
-					<p>Popular Tags</p>
-					<div class="tag-list">
-						{#each tags as tag}
-							<a href="/?tag={tag}" rel="prefetch" class="tag-default tag-pill"> {tag} </a>
-						{/each}
-					</div>
-				</div>
+			<div class="col-md-12" style="text-align: center; padding: 2rem;">
+				<h2>Welcome!</h2>
+				<p>This is a simplified SvelteKit application .</p>
+				
+				{#if $session.user}
+					<p>You are logged in as: <strong>{$session.user.username}</strong></p>
+					<a href="/settings" class="btn btn-primary">Settings</a>
+				{:else}
+					<p>Please sign in to continue.</p>
+					<a href="/login" class="btn btn-primary">Sign In</a>
+					<a href="/register" class="btn btn-secondary" style="margin-left: 1rem;">Sign Up</a>
+				{/if}
 			</div>
 		</div>
 	</div>
