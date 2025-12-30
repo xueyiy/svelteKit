750951811ac11cce6327ae66edd3c64c8b1a42a0
xueyiyang <xueyiyang@smartz.cloud>
2025-12-30 14:08:59 -0600

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,21 @@
-import netlify from '@sveltejs/adapter-netlify';
+import node from '@sveltejs/adapter-node';
 import vercel from '@sveltejs/adapter-vercel';
+import netlify from '@sveltejs/adapter-netlify';
+
+const isVercel = !!process.env.VERCEL;
+const isNetlify = !!process.env.NETLIFY;
+const isDocker = !isVercel && !isNetlify;
 
 export default {
-	kit: {
-		adapter: process.env.VERCEL ? vercel() : netlify(),
-		target: '#svelte'
-	}
+  kit: {
+    adapter: isVercel
+      ? vercel()
+      : isNetlify
+      ? netlify()
+      : node({
+          out: 'build',
+          precompress: true
+        })
+  }
 };
+
